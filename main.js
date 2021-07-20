c4d08feacc6a5ec86f6e5c06845ef19cd9e30efb
Detective-Wasif <81944371+Detective-Wasif@users.noreply.github.com>
2021-07-20 21:17:53 +0600

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ function range(stop,start=0,step=1){
   return [...Array(stop-start).keys()].fill(start).map((x,i)=>x+i*step).filter(x=>x<stop)
 }
 
+function chunk(size){
+  return Array(Math.ceil(this.length/size)).fill().map((_,i)=>i*size).map(begin=> this.slice(begin,begin+size))
+}
 
 Array.prototype.transpose=transpose
+Array.prototype.chunk=chunk
 
+// Tests below
 console.log(JSON.stringify([[1,2,3],[1,2,3],[1,2,3]].transpose()))
+console.log(JSON.stringify([1,2,3,4,5].chunk(2)))
