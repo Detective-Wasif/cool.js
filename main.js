aff03c368b1756f9f98411d19cb6c2524cf65f1e
Detective-Wasif <81944371+Detective-Wasif@users.noreply.github.com>
2021-07-20 20:52:11 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function transpose(){
-  return this[0].map((_,i)=>this.map(r=>[r[i]]))
+  return this[0].map((_,i)=>this.map(r=>r[i]))
 }
 
 function range(stop,start=0,step=1){
   return [...Array(stop-start).keys()].fill(start).map((x,i)=>x+i*step).filter(x=>x<stop)
 }
 
+
 Array.prototype.transpose=transpose
+
+console.log(JSON.stringify([[1,2,3],[1,2,3],[1,2,3]].transpose()))
