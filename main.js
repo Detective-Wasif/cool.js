7ac1a8f68b786c3b107894e69ac01d1e4976c5e2
Detective-Wasif <81944371+Detective-Wasif@users.noreply.github.com>
2021-07-20 20:34:38 +0600

    Main

---
+++ main.js
@@ -0,0 +1,9 @@
+function transpose(){
+  return this[0].map((_,i)=>this.map(r=>[r[i]]))
+}
+
+function range(stop,start=0,step=1){
+  return [...Array(stop-start).keys()].fill(start).map((x,i)=>x+i*step).filter(x=>x<stop)
+}
+
+Array.prototype.transpose=transpose
